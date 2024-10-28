c117c87038a60bfe8c4b6c41388825df19629690
Eshaan <eshaankaranth06@gmail.com>
2024-10-28 21:36:33 +0530

    a lot

--- backend/orders_dao.py
+++ backend/orders_dao.py
@@ -5,7 +5,7 @@ def insert_order(connection, order):
     cursor = connection.cursor()
 
     order_query = ("INSERT INTO orders "
-             "(customer_name, total, datetime)"
+             "(customer_name,total, date_time)"
              "VALUES (%s, %s, %s)")
     order_data = (order['customer_name'], order['grand_total'], datetime.now())
 
@@ -62,40 +62,18 @@ def get_all_orders(connection):
     query = ("SELECT * FROM orders")
     cursor.execute(query)
     response = []
-    for (order_id, customer_name, total, dt) in cursor:
+    for (order_id, customer_name, total, date_time) in cursor:
         response.append({
             'order_id': order_id,
             'customer_name': customer_name,
             'total': total,
-            'datetime': dt,
+            'date_time': date_time
         })
-
-    cursor.close()
-
-    # append order details in each order
-    for record in response:
-        record['order_details'] = get_order_details(connection, record['order_id'])
 
     return response
 
 if __name__ == '__main__':
     connection = get_sql_connection()
     print(get_all_orders(connection))
-    # print(get_order_details(connection,4))
-    # print(insert_order(connection, {
-    #     'customer_name': 'dhaval',
-    #     'total': '500',
-    #     'datetime': datetime.now(),
-    #     'order_details': [
-    #         {
-    #             'product_id': 1,
-    #             'quantity': 2,
-    #             'total_price': 50
-    #         },
-    #         {
-    #             'product_id': 3,
-    #             'quantity': 1,
-    #             'total_price': 30
-    #         }
-    #     ]
-    # }))
+   
+   
